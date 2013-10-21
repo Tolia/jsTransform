2f4fb3f49910fe812bd5ccc64a69a1d7953ffff0
tolia <toliademidov@gmail.com>
2013-10-21 14:15:36 +0400

    update

--- js/transform.js
+++ js/transform.js
@@ -1,40 +1,102 @@
 (function( $ ){
-  var css2, css3, cssTranslate, prefixTransform, typeOfNumber, vendor;
+
+  var Css3PositionController, OldCssPosition, prefixTransform, vendor, _ref,
+  __hasProp = {}.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
   vendor = /webkit/i.test(navigator.appVersion) ? 'webkit' : /firefox/i.test(navigator.userAgent) ? 'Moz' : 'opera' in window ? 'O' : '';
 
   prefixTransform = vendor + "Transform";
 
-  typeOfNumber = function(val) {
-    return typeof val === "number";
+  Css3PositionController = (function() {
+    function Css3PositionController(el) {
+      this.el = el;
+    }
+
+    Css3PositionController.prototype.moveTo = function(x, y) {
+      var position;
+      position = this.getPosition();
+      if (typeof x === "number") {
+        position.x = x;
+      }
+      if (typeof y === "number") {
+        position.y = y;
+      }
+      return this.setPosition(position.x, position.y);
+    };
+
+    Css3PositionController.prototype.moveOn = function(x, y) {
+      var position;
+      position = this.getPosition();
+      if (typeof x === "number") {
+        position.x = position.x + x;
+      }
+      if (typeof y === "number") {
+        position.y = position.y + y;
+      }
+      return this.setPosition(position.x, position.y);
+    };
+
+    Css3PositionController.prototype.getPosition = function() {
+      var position, transform;
+      transform = this.el.style[prefixTransform];
+      position = !!transform ? transform.match(/-?[0-9.0-9]+/gi) : [0, 0];
+      return {
+        x: parseInt(position[0]),
+        y: parseInt(position[1])
+      };
+    };
+
+    Css3PositionController.prototype.setPosition = function(x, y) {
+      return this.el.style[prefixTransform] = "translate(" + position.x + "px," + position.y + "px)";
+    };
+
+    return Css3PositionController;
+
+  })();
+
+  OldCssPosition = (function(_super) {
+    var getPosition, setPosition;
+
+    __extends(OldCssPosition, _super);
+
+    function OldCssPosition() {
+      _ref = OldCssPosition.__super__.constructor.apply(this, arguments);
+      return _ref;
+    }
+
+    getPosition = function() {
+      return {
+        x: parseInt(this.el.style["left"]),
+        y: parseInt(this.el.style["top"])
+      };
+    };
+
+    setPosition = function(x, y) {
+      if (typeOfNumber(x)) {
+        this.el.style["left"] = x + "px";
+      }
+      if (typeOfNumber(y)) {
+        return this.el.style["top"] = y + "px";
+      }
+    };
+
+    return OldCssPosition;
+
+  })(Css3Position);
+
+  jQuery.fn.animatedTranslate = function(params) {
+    var controllPosition;
+    controllPosition = this.data("controllPosition");
+    if (!controllPosition) {
+      controllPosition = transformWork ? new Css3Position(this[0]) : new OldCssPosition(this[0]);
+      this.data("controllPosition", controllPosition);
+    }
+    if (params["add"]) {
+      return controllPosition.moveOn(params.x, params.y);
+    } else {
+      return controllPosition.moveTo(params.x, params.y);
+    }
   };
 
-  css2 = function(x, y) {
-    if (typeOfNumber(x)) {
-      this.style["left"] = x + "px";
-    }
-    if (typeOfNumber(y)) {
-      return this.style["top"] = y + "px";
-    }
-  };
-
-  css3 = function(x, y) {
-    var newPoz;
-    newPoz = this.style[prefixTransform].match(/-?[0-9.0-9]+/gi) || [0, 0];
-    if (typeOfNumber(x)) {
-      newPoz[0] = x;
-    }
-    if (typeOfNumber(y)) {
-      newPoz[1] = y;
-    }
-    return this.style[prefixTransform] = "translate(" + newPoz[0] + "px," + newPoz[1] + "px)";
-  };
-
-  cssTranslate = prefixTransform in document.documentElement.style ? css3 : css2;
-
-  $.fn.animatedTranslate = function(params) {
-    return this.each(function() {
-      return cssTranslate.call(this, params.x, params.y);
-    });
-  };
 })( jQuery );
--- js/transform.js.coffee
+++ js/transform.js.coffee
@@ -3,18 +3,45 @@ vendor =  if (/webkit/i).test(navigator.appVersion) then 'webkit' else
               if 'opera' of window then 'O' else ''
 
 prefixTransform = vendor + "Transform"
-typeOfNumber = (val) -> typeof(val) == "number"
-css2 = (x,y) ->
-  @style["left"] = x + "px" if typeOfNumber(x)
-  @style["top"] = y + "px" if typeOfNumber(y)
-css3 = (x,y) ->
-  newPoz = @style[prefixTransform].match(/-?[0-9.0-9]+/gi) || [0,0]
-  newPoz[0] = x if typeOfNumber(x)
-  newPoz[1] = y if typeOfNumber(y)
-  @style[prefixTransform] = "translate(#{newPoz[0]}px,#{newPoz[1]}px)"
-cssTranslate = if prefixTransform of document.documentElement.style then css3 else css2
 
-$.fn.animatedTranslate = (params) ->
-  @.each ->
-    cssTranslate.call @, params.x, params.y
+class Css3PositionController
+  constructor: (@el) ->
+  moveTo: (x,y) ->
+    position = @getPosition()
+    position.x = x if typeof(x) == "number"
+    position.y = y if typeof(y) == "number"
+    @setPosition position.x, position.y
+  moveOn: (x,y) ->
+    position = @getPosition()
+    position.x = position.x + x if typeof(x) == "number"
+    position.y = position.y + y if typeof(y) == "number"
+    @setPosition position.x, position.y
+  getPosition: ->
+    transform = @el.style[prefixTransform]
+    position = if !!transform then transform.match(/-?[0-9.0-9]+/gi) else [0,0]
+    return {
+      x: parseInt position[0]
+      y: parseInt position[1]
+    } 
+  setPosition: (x,y) ->
+    @el.style[prefixTransform] = "translate(#{position.x}px,#{position.y}px)"
 
+class OldCssPosition extends Css3Position
+  getPosition = ->
+    x: parseInt @el.style["left"]
+    y: parseInt @el.style["top"]
+  setPosition = (x,y) ->
+    @el.style["left"] = x + "px" if typeOfNumber(x)
+    @el.style["top"] = y + "px" if typeOfNumber(y)
+
+jQuery.fn.animatedTranslate = (params) ->
+  controllPosition = @data "controllPosition"
+  
+  if !controllPosition
+    controllPosition = if transformWork then new Css3Position @[0] else new OldCssPosition @[0]
+    @data "controllPosition", controllPosition
+  
+  if params["add"]
+    controllPosition.moveOn params.x, params.y
+  else
+    controllPosition.moveTo params.x, params.y
